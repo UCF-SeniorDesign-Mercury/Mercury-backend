52feca56e279d1bebdf28d267d0019c52685630d
junqili <junqili259@gmail.com>
2021-03-17 04:05:46 -0400

    refactored code

--- app.py
+++ app.py
@@ -1,59 +1,46 @@
 from firebase_admin import credentials, auth, firestore, initialize_app
 from flask import Flask, Response, request, jsonify
 from functools import wraps
+from decorators import check_token
 
 app = Flask(__name__)
 cred = credentials.Certificate('key.json')
 firebase_app = initialize_app(cred)
 db = firestore.client()
 
-def check_token(f):
-    """
-    This decorator verifies user idToken for protected routes so that only valid idTokens may access them
-    """
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        """
-        Receives idToken from the front-end and attempts to verify idToken and if successful, then access to protected route is granted.
-        If the idToken is invalid for any reason whether it's expired or revoked, a corresponding error message is returned instead and access to protected route is denied.
-        """
-
-        #token = request.args.get('token')
-
-        #print(request.headers)
-        token = request.headers['Authorization']
-
-        try:
-            auth.verify_id_token(token, check_revoked=True)
-
-        except auth.ExpiredIdTokenError:
-            return jsonify({'Error message': 'Token expired'}), 403
-
-        except auth.InvalidIdTokenError:
-            return jsonify({'Error message': 'Invalid token'}), 403
-
-        except auth.RevokedIdTokenError:
-            return jsonify({'Error message': 'Token revoked'}), 403
-        
-        except:
-            try:
-                token = request.args.get('token')
-                auth.verify_id_token(token, check_revoked=True)
-            except:
-                return jsonify({'Error message': 'Invalid Token'}), 403
-
-        return f(*args, **kwargs)
-    return wrap
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Hello World"
 
-@app.route('/test', methods=['GET'])
+
+@app.route('/grantRole', methods=['GET'])
 @check_token
-def test():
-    return jsonify({"Message": "It works!!!"})
+def grantRole():
+    """
+    Upon newly registering a user, they are granted a role if their email used is matched to one in the database. 
+    """
 
+    # Decode token to obtain user's firebase id
+    token = request.headers['Authorization']
+    decoded_token = auth.verify_id_token(token)
+    
+    email = decoded_token['email']
+    uid = decoded_token['uid']
+
+    # Reference to roles document
+    doc_ref = db.collection(u'Roles').document(u'roleList')
+    doc = doc_ref.get()
+
+    roles = {}
+    if doc.exists:
+        roles = doc.to_dict()['role']
+
+    try:
+        auth.set_custom_user_claims(uid, {roles[email]: True})
+        return jsonify({"Message": "It works!!!"})
+    except:
+        return jsonify({"Message": "Not assigned a role"})
+
+
+# 
 if __name__ == '__main__':
     app.run(host='192.168.1.13', debug=True)
---
+++ decorators.py
@@ -0,0 +1,40 @@
+from functools import wraps
+from flask import request, jsonify
+from firebase_admin import auth
+
+def check_token(f):
+    """
+    This decorator verifies user idToken for protected routes so that only valid idTokens may access them
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        """
+        Receives idToken from the front-end and attempts to verify idToken and if successful, then access to protected route is granted.
+        If the idToken is invalid for any reason whether it's expired or revoked, a corresponding error message is returned instead and access to protected route is denied.
+        """
+        if 'Authorization' not in request.headers:
+            return jsonify({'Error Message': 'Authorization token not provided'})
+
+        token = request.headers['Authorization']
+
+        try:
+            auth.verify_id_token(token, check_revoked=True)
+
+        except auth.ExpiredIdTokenError:
+            return jsonify({'Error message': 'Token expired'}), 403
+
+        except auth.InvalidIdTokenError:
+            return jsonify({'Error message': 'Invalid token'}), 403
+
+        except auth.RevokedIdTokenError:
+            return jsonify({'Error message': 'Token revoked'}), 403
+        
+        except:
+            try:
+                token = request.args.get('token')
+                auth.verify_id_token(token, check_revoked=True)
+            except:
+                return jsonify({'Error message': 'Invalid Token'}), 403
+
+        return f(*args, **kwargs)
+    return wrap
