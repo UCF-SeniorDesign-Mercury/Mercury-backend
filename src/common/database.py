d0aa30381ac12c816705027e7b7ce5f073b2e3e3
TangyrenMidKing <d1029433728@gmail.com>
2021-12-14 18:45:55 -0500

    change the way how to get .evn vars

--- src/common/database.py
+++ src/common/database.py
@@ -4,19 +4,21 @@
     ~~~~~~~~~~~~~~~~~~~
 """
 from firebase_admin import credentials, firestore, initialize_app, storage
-from os import environ
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 FIREBASE_KEYS = {
     "type": "service_account",
-    "project_id": environ["FIREBASE_PROJECT_ID"],
-    "private_key_id": environ["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
-    "client_email": environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": environ["FIREBASE_CLIENT_ID"],
-    "auth_uri": environ["FIREBASE_AUTH_URI"],
-    "token_uri": environ["FIREBASE_TOKEN_URI"],
-    "auth_provider_x509_cert_url": environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
-    "client_x509_cert_url": environ["FIREBASE_CLIENT_X509_CERT_URL"],
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
 }
 
 cred = credentials.Certificate(FIREBASE_KEYS)
