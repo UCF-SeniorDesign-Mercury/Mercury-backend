c3ea28ac18ce33fe5c9f2c3f4de7a09e45773bb5
Matthew Dahbar <matthewdahbar@gmail.com>
2022-10-22 21:59:37 -0400

    Update rst.py

--- src/api/rst.py
+++ src/api/rst.py
@@ -116,7 +116,7 @@ def upload_rst_data() -> Response:
     # exceptions
     if "filename" in data and data.get("filename") == "":
         return BadRequest("Missing the filename")
-    if !(data["filename"].endswith(".xlsx")):
+    if (data["filename"].endswith(".xlsx") == 0):
         return UnsupportedMediaType("The endpoint only accept .xlsx file")
     if "xlsx_file" in data and data.get("xlsx_file") == "":
         return BadRequest("Missing the xlsx_file")
